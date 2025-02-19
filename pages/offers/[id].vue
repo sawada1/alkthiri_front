e73a0ab800e723077f04187bb653d6c0f93726e0
khaled sawada <94247954+sawada1@users.noreply.github.com>
2025-02-19 13:12:15 +0200

    test

---
+++ pages/offers/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-screen">
+        {{ $route.params.id }}
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
---
+++ pages/offers/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen container mt-10">
+    <div class="flex items-center gap-5">
+        <nuxt-link v-for="i in arr" :to="`/offers/${i}`"> test {{ i }}</nuxt-link>
+    </div>
+    </div>
+</template>
+<script setup>
+  let arr = ref([1 , 2 , 3 , 4 , 5]);
+</script>
+<style lang="">
+    
+</style>
