a87672a9c8c4df925d9e945bc0dafbbc4d2b6ee9
khaled sawada <94247954+sawada1@users.noreply.github.com>
2025-04-23 15:00:34 +0200

    tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,7 +100,23 @@ export default defineNuxtConfig({
    
       ],
       script: [
-        
+        {
+          async:true,
+          innerHTML: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQN2K7PF');
+        `,
+        },
+        {
+          async:true,
+          innerHTML: `
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQN2K7PF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        `,
+        },
       ],
     }
   },
